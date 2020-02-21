88f23e69699fabc481c7e347240a2f09ee9bfd06
Steven Ondieki omwange <ondiekisteven@users.noreply.github.com>
2020-02-21 08:41:12 +0300

    fixed slash in profile url

--- list/urls.py
+++ list/urls.py
@@ -4,7 +4,7 @@ from users.views import profile
 
 urlpatterns = [
     path('', index, name='auth-list'),
-    path('add-item/', add_item, name='add-item'),
+    path('add-item', add_item, name='add-item'),
     path('delete-item/<int:item_id>', delete_item),
     path('api/', list_items),
     path('api/<int:item_id>', get_item),
